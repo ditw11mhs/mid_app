b8646dca150f8f28f933fd32c95a839f927043d2
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-06-22 11:52:05 +0700

    Commit for another tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:mid_app/ans.dart';
-import 'package:mid_app/question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _i = 0;
-  var _texts = ['oh','there','you','are','I','just','wanna','say','I', 'Love', 'You'];
-
-  void _next() {
-    setState(() {
-      if (_i < _texts.length-1) {
-        _i++;
-      } else {
-        _i = 0;
-      }
-    });
-
-    print(_i);
-  }
-
-  void _before() {
-    setState(() {
-      if (_i > 0) {
-        _i--;
-      } else {
-        _i = _texts.length-1;
-      }
-    });
-
-    print(_i);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Question(_texts[_i]),
-            Answer('Next', _next),
-            Answer('Before',_before)
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            titleTextStyle: TextStyle(fontStyle: FontStyle.italic),
+            title: Text('Test'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Text1',
+              ),
+              Text('Text2'),
+              Text('Text3'),
+              Row(
+                children: <Widget>[Text('text4'), Text('text5')],
+              )
+            ],
+          )),
     );
   }
 }
