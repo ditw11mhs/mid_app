0d1151dbd35921fe0584b34fb917f12fa7fda020
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-07-24 00:01:12 +0700

    Retest Things

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mid_app/LoginPage.dart';
+import 'font_style.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,72 +10,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'My App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('My App'),
+            backgroundColor: Colors.red,
+          ),
+          body: SafeArea(
+            child: Center(
+              child: Container(
+                // color: Colors.blue,
+                margin: EdgeInsets.only(
+                    left: 10.0, top: 5.0, right: 10.0, bottom: 5.0),
+                padding: EdgeInsets.only(
+                    left: 10.0, top: 5.0, right: 10.0, bottom: 5.0),
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/test.png'),
+                      height: 200,
+                    ),
+                    Text(
+                      'Hello, world!',
+                      style: Header,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
-
-// class MyApp extends StatefulWidget {
-//   MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   int num = 10;
-
-//   void addition() {
-//     setState(() {
-//       num += 10;
-//     });
-//   }
-
-//   void subtraction() {
-//     setState(() {
-//       num -= 10;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Test'),
-//         ),
-//         body: Center(
-//           child: ListView(
-//             children: <Widget>[
-//               for (var i = 0; i < num; i++) Text('Item $i'),
-//               Center(
-//                   child: Text(
-//                 num.toString(),
-//                 style: TextStyle(fontSize: 10 + num.toDouble()),
-//               )),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                 children: <Widget>[
-//                   ElevatedButton(
-//                     onPressed: addition,
-//                     child: Text(
-//                       'Add',
-//                       style: GoogleFonts.roboto(
-//                           textStyle: TextStyle(
-//                         color: Colors.blue[50],
-//                       )),
-//                     ),
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: subtraction,
-//                     child: Text('Subtract'),
-//                   )
-//                 ],
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
